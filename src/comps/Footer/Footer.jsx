eab4c3fc426fe19f8137f77a0b80b20ccc2857d7
LGBesports <david17roma@gmail.com>
2022-11-16 12:21:02 +0300

    footerbag

--- src/comps/Footer/Footer.jsx
+++ src/comps/Footer/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
                 <div className="footer__wrapper">
                     <div className="footer__left">2022 © “Новелти диагностика и терапия”</div>
                     <div className="footer__right">
-                        <div className="footer__loc"> <Map /> Москва, Филевский бульвар, <br /> д. 39, офис 4</div>
+                        <div className="footer__loc"> <Map /> <p>Москва, Филевский бульвар, <br /> д. 39, офис 4</p> </div>
                         <div className="footer__contacts">
                             <a className='tel' href="tel:+74951184048">+7 495 118 40 48 </a>
                             <a className='mail' href="mailto:info@novelty.express">info@novelty.express </a>
